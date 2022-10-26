0998dde14f1b8bb344885803d22e1675741a84e6
pcy <cas15987@gmail.com>
2022-10-26 11:23:14 +0900

    회원 등록 API

---
+++ springboot-web&rest-api/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,70 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+//@Controller @ResponseBody
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    /**
+     * 등록 V1: 요청 값으로 Member 엔티티를 직접 받는다.
+     *
+     * 문제점
+     *  - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
+     *  - 엔티티에 API 검증을 위한 로직이 들어간다. (@NotEmpty 등등)
+     *  - 실무에서는 회원 엔티티를 위한 API가 다양하게 만들어지는데, 한 엔티티에 각각의 API를 위한 모든 요청 요구사항을 담기는 어렵다.
+     *  - 엔티티가 변경되면 API 스펙이 변한다.
+     *
+     * 결론
+     *  - API 요청 스펙에 맞추어 별도의 DTO를 파라미터로 받는다.
+     */
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    /**
+     * 등록 V2: 요청 값으로 Member 엔티티 대신에 별도의 DTO를 받는다.
+     *
+     * 장점
+     *  - API 스펙이 변경되지 않는다.
+     *  - 클라이언트로부터 넘어오는 파라미터가 명확해진다.
+     *  - API 스펙에 맞게 Validation을 설정할 수 있다.
+     *
+     * 결론
+     *  - 유지보수할 때 장점이 있다.
+     */
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+
+        memberService.join(member);
+
+        return new CreateMemberResponse(member.getId());
+
+    @Data
+    static class CreateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
