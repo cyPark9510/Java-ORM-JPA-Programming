3adc918a9664bf19273f7a200f596d1d3b9b60a6
pcy <cas15987@gmail.com>
2022-10-26 11:23:26 +0900

    회원 수정 API

--- springboot-web&rest-api/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ springboot-web&rest-api/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -53,6 +53,16 @@ public class MemberApiController {
         memberService.join(member);
 
         return new CreateMemberResponse(member.getId());
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
 
     @Data
     static class CreateMemberRequest {
@@ -66,5 +76,17 @@ public class MemberApiController {
         public CreateMemberResponse(Long id) {
             this.id = id;
         }
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
     }
 }
