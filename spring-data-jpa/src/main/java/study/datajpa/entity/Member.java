934c40634dea9d1a2c5edf23fc776c572ca0ea39
pcy <cas15987@gmail.com>
2022-11-02 19:57:34 +0900

    프로젝트 환경설정

---
+++ spring-data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,24 @@
+package study.datajpa.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+
+    protected Member() {
+    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+}
